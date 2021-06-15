011fa03076f91749e2141b6cb0776ef9cfdba2af
rEq10000 <rarz11@yahoo.com>
2021-06-16 02:04:41 +0300

    Main file

---
+++ IPTVCap.py
@@ -0,0 +1,17 @@
+import datetime
+import os
+import time
+
+os.system("title IPTVCap - Images Downloaded: 0")
+
+iptvlink = "linkhere"
+curvalue = 0
+while True:
+	day = datetime.datetime.now().strftime('-%Y-%m-%d %H.%M.%S')
+
+	channel = "channelnamehere"
+
+	os.system('ffmpeg -i "' + iptvlink + '" -vf fps=1/1 -frames:v 1 "' + channel + day + '.jpg' + '"')
+	curvalue = 1 + curvalue
+	os.system("title IPTVCap - Images Downloaded: " + curvalue)
+	time.sleep(60)
